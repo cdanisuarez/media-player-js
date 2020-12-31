0195951b915852674c93eaa41030f4a2e4717511
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2020-12-31 10:44:15 -0500

    Scopes examples added: global, funcion, block and module

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+import MediaPlayer from './media-player.js';
+
+const video = document.querySelector('video');
+const button = document.querySelector('button');
+const player = new MediaPlayer({ el: video });
+
+button.onclick = () => player.click();
