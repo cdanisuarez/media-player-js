67145d381ce2e7f02a994f3167235e0b3efc867a
Cindy Daniela Suarez <cindy.suarez@prodigious.com>
2021-02-01 16:20:39 -0500

    this examples added

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ import AutoPlay from './plugins/auto-play.js';
 const video = document.querySelector('video');
 const playButton = document.querySelector('#play');
 const playMute = document.querySelector('#mute');
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({ el: video, /*plugins: [new AutoPlay()]*/ });
 
 playButton.onclick = () => player.play();
 playMute.onclick = () => player.mute();
